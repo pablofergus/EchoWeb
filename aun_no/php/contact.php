1e26b56193e71a567a5ae1729b91b9cd0d33ace9
Pablo Fergus <pablofergus@gmail.com>
2016-03-05 12:55:02 +0100

    Some stuff

---
+++ aun_no/php/contact.php
@@ -0,0 +1,56 @@
+<?php
+if(isset($_POST['email'])) {
+     
+    // CHANGE THE TWO LINES BELOW
+    $email_to = "pablofergus@gmail.com";
+     
+    $email_subject = "Mensaje via echoband.es";
+     
+     
+    function died($error) {
+        // your error code can go here
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
+        echo $error."<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
+        die();
+    }
+     
+    // validation expected data exists
+    if(!isset($_POST['comments'])) {
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
+    }
+
+    $comments = $_POST['comments']; // required
+     
+    $error_message = "";
+    $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
+  if(strlen($comments) < 2) {
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
+  }
+  if(strlen($error_message) > 0) {
+    died($error_message);
+  }
+    $email_message = "Form details below.\n\n";
+     
+    function clean_string($string) {
+      $bad = array("content-type","bcc:","to:","cc:","href");
+      return str_replace($bad,"",$string);
+    }
+
+    $email_message .= "Comments: ".clean_string($comments)."\n";
+     
+     
+// create email headers
+$headers = 'X-Mailer: PHP/' . phpversion();
+@mail($email_to, $email_subject, $email_message, $headers);  
+?>
+ 
+<!-- place your own success html below -->
+ 
+Gracias por contactar con nosotros.
+ 
+<?php
+}
+die();
+?>
